c771fff4255b7d4595c0af9def451ff52108ce22
C W <claudiaywong@msn.com>
2021-11-17 20:01:37 +0000

    finished remaining specs

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,34 +1,34 @@
+
+   
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "They can navigate from the home page to the product detail page by clicking on a product", type: :feature, js: true do
 
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
     10.times do |n|
+      @sample_product_name = Faker::Hipster.sentence(3)
+
       @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
+        name:  @sample_product_name,
         description: Faker::Hipster.paragraph(4),
         image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
     end
+
   end
 
   scenario "They can navigate from the home page to the product detail page by clicking on a product" do
     visit root_path
-    # find('article.product', match: :first).click
-    find('p.description', match: :first).click
-    # commented out b/c it's for debugging only
-    # save_and_open_screenshot
-    # DEBUG
+
+    click_link(@sample_product_name)
+
+    expect(page).to have_content("Apparel")
+
     save_screenshot
-    puts page.html
-    #expect(page).to have_css 'section.products-show'
-    within('main') do
-      expect(page).to have_css("section.products-show")
-    end
   end
 end
